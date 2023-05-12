0af114decc0ab7a6ef4d96be2abe14cfd81f93eb
jewelrana3 <juyelrana7752@gmail.com>
2023-05-12 08:38:50 +0600

    last commit

--- index.js
+++ index.js
@@ -50,6 +50,27 @@ async function run() {
         res.send(result)
     });
 
+    app.put('/coffee/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id:new ObjectId(id)}
+      const options = { upsert: true };
+      const updateUser = req.body;
+      
+      const coffee = {
+        $set:{
+          name:updateUser.name, 
+          quantity:updateUser.quantity, 
+          supplier:updateUser.supplier, 
+          taste:updateUser.taste, 
+          catacogy:updateUser.catacogy, 
+          details:updateUser.details, 
+          photo:updateUser.photo,
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter,options,coffee)
+      res.send(result)
+    })
+
     app.delete('/coffee/:id',async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
